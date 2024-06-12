555ee31cd77ef63664960c0ee32ba9c3e35fa18b
Dani <danielmartin.hermoso.hermoso.alu@iesfernandoaguilar.es>
2024-06-12 21:18:19 +0200

    Proyecto terminado

--- lanzarSistema.sh
+++ lanzarSistema.sh
@@ -7,7 +7,7 @@ sudo docker pull hehedaniel/backcompleto:backfuncional
 sudo docker network create --driver bridge --subnet 172.79.0.0/16 --gateway 172.79.0.1 redApi
 
 # Creas el contenedor en base a la imagen que descargamos anteriormente
-sudo docker run -d --name backXampp -p 8080:80 -p 8081:3306 -p 8000:8000 --ip 172.79.0.3 --networ redApi -v ./WellnessTrack-api:/home/docker --entrypoint /bin/sh hehedaniel/backcompleto:backfuncional -c "/opt/lampp/lampp start && cd /home/docker/Wellnesstrack-api && symfony server:start --port=8000"
+sudo docker run -d --name backXampp -p 8080:80 -p 8081:3306 -p 8000:8000 --ip 172.79.0.3 --networ redApi -v ./WellnessTrack-api:/home/docker --entrypoint /bin/sh hehedaniel/backcompleto:backfinal -c "/opt/lampp/lampp start && cd /home/docker/Wellnesstrack-api && symfony server:start --port=8000"
 
 cd WellnessTrack-angular
 npm install
